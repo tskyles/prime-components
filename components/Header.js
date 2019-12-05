95582a484238269e3eefffee2628abe51fceb46d
travis skyles <travis.skyles@gmail.com>
2019-12-05 15:56:59 -0800

    linked assets folder and created Header.js

---
+++ components/Header.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components/native';
+
+export default styled.Text`
+  color: black;
+  font-family: RacingSansOne-Regular;
+  font-size: 32px;
+  margin-top: 120px;
+  background-color: transparent;
+  text-align: center;
+`;
